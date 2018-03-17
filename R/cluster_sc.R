06e73f8c06253bdb7de4e3a9b12a102e64348ebb
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-03-17 13:43:34 -0400

    spec cluster

--- R/cluster_sc.R
+++ R/cluster_sc.R
@@ -11,8 +11,13 @@
 #' clusters <- cluster_sc(input = exprs(ex_sc_example), method = "spectral", num_clust)
 
 cluster_sc <- function(input, method, num_clust){
-  spec <- kknn::specClust(prelim_dims_PCA, centers = num_clust, method = 'random-walk')
-  return(spec$cluster)
+  if(method == "spectral"){
+    spec <- kknn::specClust(prelim_dims_PCA, centers = num_clust, method = 'random-walk')
+    cluster <- spec$cluster
+  }
+  if(method == "density"){
+  }
+  return(cluster)
 }
 
 
