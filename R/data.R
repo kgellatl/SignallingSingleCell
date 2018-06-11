df53b060c64c93c13979beca74a2d7c5231a598f
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-06-11 14:51:22 -0400

    check_package function for documentation

--- R/data.R
+++ R/data.R
@@ -6,6 +6,7 @@
 #' Self note... Create .rda file in /data, then a .R file called data
 #' run devtools::load_all()
 #' run roxygen2::roxygenise()
+#' or run SignallingSingleCell::check_package()
 #'
 #' @references Ramilowski, Jordan A., et al. "A draft network of ligand–receptor-mediated multicellular signalling in human." Nature communications 6 (2015): 7866.
 #'
