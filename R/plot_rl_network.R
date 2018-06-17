e2f0b8c757843f7b9decd1f40318190ea6256413
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-06-16 20:20:51 -0400

    Completed Summary network plots

--- R/plot_rl_network.R
+++ R/plot_rl_network.R
@@ -13,15 +13,14 @@
 #' ex_sc_example <- id_rl(input = ex_sc_example)
 
 plot_rl_network <- function(input, group_by = FALSE, mode = "Summary"){
-
+  ##### Colors to match ggplot #####
   gg_color_hue <- function(n) {
     hues = seq(15, 375, length = n + 1)
     hcl(h = hues, l = 65, c = 100)[1:n]
   }
-
   n = length(unique(as.character(input$Summary$V1)))
   dynamic_colors = gg_color_hue(n)
-
+  ##### Arrowhead Sizing #####
   eqarrowPlot <- function(graph, layout, edge.lty=rep(1, ecount(graph)),
                           edge.arrow.size=rep(1, ecount(graph)),
                           vertex.shape="circle",
@@ -38,34 +37,84 @@ plot_rl_network <- function(input, group_by = FALSE, mode = "Summary"){
          vertex.shape=vertex.shape, edge.color=edge.col, add=TRUE, ...)
     invisible(NULL)
   }
-
-  ######
-
+  ##### Summary plot type is edges weighted by the number of connections #####
   if(mode == "Summary"){
     net_dat <- input$Summary
     for (i in 1:ncol(net_dat)) {
       net_dat[,i] <- as.character(net_dat[,i])
     }
-    network <- c()
-    if(group_by!= FALSE){
-      groups <- unique(net_dat$V3)
+    if(group_by!= FALSE){  ##### GROUPED DATA #####
+      net_dat_final <- net_dat
+      for (i in 1:nrow(net_dat_final)) {
+        tmp <- net_dat_final[i,]
+        tmp$V1 <- paste0(c(tmp$V3, tmp$V1), collapse = "_")
+        tmp$V2 <- paste0(c(tmp$V3, tmp$V2), collapse = "_")
+        net_dat_final[i,] <- tmp
+      }
+      for (i in 1:nrow(net_dat_final)) {
+        tmp <- net_dat_final[i,]
+        tmp$V1 <- paste0(c("Lig", tmp$V1), collapse = "_")
+        tmp$V2 <- paste0(c("Rec", tmp$V2), collapse = "_")
+        net_dat_final[i,] <- tmp
+      }
+      order <- c()
+      groups  <- unique(net_dat$V3)
       for (i in 1:length(groups)) {
-        int <- groups[i]
-        tab <- net_dat[grep(int, net_dat$V3),1:3]
-        for(j in 1:nrow(tab)){
-          int2 <- tab[j,]
-          tab[j,1] <- paste0(c(int2$V1, int2$V3), collapse = "_")
-          tab[j,2] <- paste0(c(int2$V2, int2$V3), collapse = "_")
-          net_dat[grep(int, net_dat$V3),1:3] <- tab
+          int <- groups[i]
+          tab <- grep(int, net_dat$V3)
+          order <- c(order , tab)
+      }
+      net_dat_final <- net_dat_final[order,]
+      net_graph <- igraph::graph_from_data_frame(net_dat_final)
+      ##### Layout #####
+      l <- layout_in_circle(net_graph)
+      rownames(l) <- names(V(net_graph))
+      y_pos <- seq(1:length(unique(net_dat$V1)))
+      l[,2] <- rep(y_pos)
+      xpos <- seq(1:(length(groups)*2))
+      for (i in 1:length(xpos)) {
+        if(i == 1){
+          # do nothing
+        } else {
+          if(i %% 2 == 0){
+            xpos[i] <- xpos[i-1]+5
+          } else {
+            xpos[i] <- xpos[i-1]+1
+          }
         }
       }
-      for (i in 1:nrow(net_dat)) {
-        add <- as.vector(net_dat[i,1:2])
-        network <- append(network, add)
+      chunk2 <- function(x,n) split(x, cut(seq_along(x), n, labels = FALSE))
+      grouped_pos <- chunk2(xpos, 3)
+      for (i in 1:length(groups)) {
+        int <- groups[i]
+        ind <- grep(int, rownames(l))
+        posits <- chunk2(ind, 2)
+        for (j in 1:length(posits)) {
+          l[posits[[j]],1] <- grouped_pos[[i]][j]
+        }
       }
-      net_graph <- igraph::graph(c(network))
-
+      ##### Color Nodes  #####
+      V(net_graph)$color <- rep(dynamic_colors, length(unique(net_dat_final$V3))*2)
+      ##### Color Edges  #####
+      edge.col <- rep(rep(dynamic_colors, each = length(unique(net_dat$V1))), length(groups))
+      ##### Size Edge and Arrows #####
+      E(net_graph)$width <- rank(as.numeric(net_dat_final$freq))
+      E(net_graph)$width <- (10/max(E(net_graph)$width)*E(net_graph)$width)
+      ##### Remove Names #####
+      V(net_graph)$name <- ""
+      ##### Size Vertices #####
+      V(net_graph)$size <- 5
+      ##### Plot #####
+      eqarrowPlot(net_graph, layout = l, edge.arrow.size=(2/max(E(net_graph)$width)*E(net_graph)$width),
+                  edge.width=E(net_graph)$size)
+      ##### Add Legend ####
+      cell_legend <- unique(net_dat$V1)
+      cell_legend <-rev(cell_legend)
+      legend(x=-1.5, y=0, cell_legend, pch=21,
+             col="#777777", pt.bg=rev(dynamic_colors), pt.cex=2, cex=.8, bty="n", ncol=1)
     } else {
+
+      ##### NON GROUPED DATA #####
       net_dat_final <- net_dat
       for (i in 1:nrow(net_dat_final)) {
         tmp <- net_dat_final[i,]
@@ -74,16 +123,15 @@ plot_rl_network <- function(input, group_by = FALSE, mode = "Summary"){
         net_dat_final[i,] <- tmp
       }
       net_graph <- igraph::graph_from_data_frame(net_dat_final)
-
       ##### Layout #####
       l <- layout_in_circle(net_graph)
-      # rownames(l) <- names(V(net_graph))
-      # ligs <- grep("Lig", rownames(l))
-      # recs <- grep("Rec", rownames(l))
-      # l[ligs,1] <- 1
-      # l[recs,1] <- 2
-      # y_pos <- seq(1:length(unique(net_dat$V1)))
-      # l[,2] <- rep(y_pos, 2)
+      rownames(l) <- names(V(net_graph))
+      ligs <- grep("Lig", rownames(l))
+      recs <- grep("Rec", rownames(l))
+      l[ligs,1] <- 1
+      l[recs,1] <- 2
+      y_pos <- seq(1:length(unique(net_dat$V1)))
+      l[,2] <- rep(y_pos, 2)
       ##### Color Nodes  #####
       V(net_graph)$color <- rep(dynamic_colors, 2)
       ##### Color Edges  #####
@@ -98,7 +146,9 @@ plot_rl_network <- function(input, group_by = FALSE, mode = "Summary"){
       E(net_graph)$width <- (10/max(E(net_graph)$width)*E(net_graph)$width)
       ##### Remove Names #####
       V(net_graph)$name <- ""
-      #####
+      ##### Size Vertices #####
+      V(net_graph)$size <- 5
+      ##### Plot #####
       eqarrowPlot(net_graph, layout = l, edge.arrow.size=(2/max(E(net_graph)$width)*E(net_graph)$width),
                   edge.width=E(net_graph)$size)
       ##### Add Legend ####
