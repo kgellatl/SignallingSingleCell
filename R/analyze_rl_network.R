f6062bb58fcce4229884dec96e0aca387b0355ba
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-07-17 15:57:29 -0400

    analyze network complete

--- R/analyze_rl_network.R
+++ R/analyze_rl_network.R
@@ -9,8 +9,47 @@
 #' @examples
 #' ex_sc_example <- id_rl(input = ex_sc_example)
 
-analyze_rl_network <- function(input){
+analyze_rl_network <- function(input, h = 5, w = 20){
+  pdf("Dendrogram.pdf", height = h, width = w)
+  dendPlot(input$communities, mode="hclust")
+  dev.off()
+  tmp_net <- input$igraph_Network
 
+  deg <- degree(tmp_net, mode="all")
+  deg <- sort(deg, decreasing = T)
+
+  deg_rec <- degree(tmp_net, mode="in")
+  deg_rec <- sort(deg_rec, decreasing = T)
+
+  deg_lig <- degree(tmp_net, mode="out")
+  deg_lig <- sort(deg_lig, decreasing = T)
+
+  vert_rank <- (betweenness(as.undirected(tmp_net))) # VERTICES
+  vert_rank <- sort(vert_rank, decreasing = T)
+
+  edg_rank <- (edge_betweenness(as.undirected(tmp_net))) # EDGES
+  tmp <- igraph::as_edgelist(tmp_net)
+  tmp2 <- apply( tmp[ , 1:2 ] , 1 , paste , collapse = "-" )
+  names(edg_rank) <- tmp2
+  edg_rank <- sort(edg_rank, decreasing = T)
+
+  hs <- hub_score(tmp_net, weights=NA)$vector #Outgoing (ligands)
+  hs <- sort(hs, decreasing = T)
+
+  as <- authority_score(tmp_net, weights=NA)$vector # Incoming (receptors)
+  as <- sort(as, decreasing = T)
+
+  results <- vector(mode = "list", length = 7)
+  results[[1]] <- deg
+  results[[2]] <- deg_rec
+  results[[3]] <- vert_rank
+  results[[4]] <- as
+  results[[5]] <- deg_lig
+  results[[6]] <- edg_rank
+  results[[7]] <- hs
+  names(results) <- c("Degree", "Node_degree", "Node_betweeness", "Node_authority",
+                      "Edge_degree", "Edge_betweeness", "Edge_hub")
+ return(results)
 }
 
 
