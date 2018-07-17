3e5d191f64db4d4dd88a58b4dab57f329fb7ef05
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-07-17 15:08:24 -0400

    adding in analysis for networks

---
+++ R/analyze_rl_network.R
@@ -0,0 +1,20 @@
+#' Identifies all R / L interactions
+#'
+#' This function will map all RL interactions
+#'
+#' @param input the input network from plot_rl_network
+#' @export
+#' @details
+#' This will use the calc_agg_bulk results to ID networks
+#' @examples
+#' ex_sc_example <- id_rl(input = ex_sc_example)
+
+analyze_rl_network <- function(input){
+
+}
+
+
+
+
+
+
