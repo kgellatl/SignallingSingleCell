5d3fab905e7dbe28d8205175b5a94443f294aa42
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-07-17 23:32:57 -0400

    removed ranked name

--- R/analyze_rl_network.R
+++ R/analyze_rl_network.R
@@ -9,20 +9,60 @@
 #' @examples
 #' ex_sc_example <- id_rl(input = ex_sc_example)
 
-analyze_rl_network <- function(input, h = 5, w = 20){
+analyze_rl_network <- function(input){
 
   tmp_net <- input
   l <- layout_nicely(tmp_net)
 
-  cfg <- cluster_edge_betweenness(as.undirected(tmp_net))
+  pdf("Fullnetwork.pdf", h = 8, w = 8, useDingbats = FALSE)
+  plot(tmp_net, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
+  dev.off()
+
+  pdf("Fullnetwork_noname.pdf", h = 8, w = 8, useDingbats = FALSE)
+  net2 <- tmp_net
+  V(net2)$name <- ""
+  plot(net2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
+  dev.off()
+
+  tmp_net_cp <- tmp_net
+  cfg <- cluster_edge_betweenness(as.undirected(tmp_net_cp))
 
   pdf("Fullnetwork_communities.pdf", h = 8, w = 8, useDingbats = FALSE)
-  plot(cfg, tmp_net, layout = l, edge.curved=curve_multiple(net_graph), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
+  plot(cfg, tmp_net_cp, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
   dev.off()
 
   pdf("Dendrogram.pdf", height = 10, width = 50)
   dendPlot(cfg, mode="hclust")
   dev.off()
+
+  #####
+  nodes <- igraph::as_data_frame(tmp_net, what = "vertices")
+  links <- igraph::as_data_frame(tmp_net, what = "edges")
+  links$arrows <- "to"
+
+  colnames(nodes)[1] <- "id"
+  nodes <- nodes[,c("id", "color")]
+
+  nodes$label <- V(tmp_net)$name
+  links$value <- E(tmp_net)$width
+
+  deg <- degree(tmp_net, mode="all")
+  deg <- rank(deg)
+  deg <- (20/max(deg)*deg)
+  deg[which(deg < 0.5)] <- 0.5
+  nodes$value <- deg
+
+  nodes$community <- cfg$membership
+
+  links$width <- 3
+
+  vit_net <- visNetwork::visNetwork(nodes, links, width="100%", height="1000px")
+
+  vit_net <- visNetwork::visOptions(vit_net, highlightNearest = TRUE, selectedBy = "community")
+
+  visNetwork::visSave(vit_net, file="Interactive_Network_analyzed.html")
+
+  #####
 
   deg <- degree(tmp_net, mode="all")
   deg <- sort(deg, decreasing = T)
@@ -57,10 +97,11 @@ analyze_rl_network <- function(input, h = 5, w = 20){
   results[[6]] <- edg_rank
   results[[7]] <- hs
   results[[8]] <- cfg
+  results[[9]] <- vit_net
 
   names(results) <- c("Degree", "Node_degree", "Node_betweeness", "Node_authority",
-                      "Edge_degree", "Edge_betweeness", "Edge_hub", "Communities")
- return(results)
+                      "Edge_degree", "Edge_betweeness", "Edge_hub", "Communities", "Interactive")
+  return(results)
 }
 
 
