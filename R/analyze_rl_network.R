ac85e954543ebaa684128dd6b8f1a81b3ec80737
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-07-18 19:53:59 -0400

    IMPORTANT CHANGES. Analyze RL can now also color edges by crossing, delete those edges, and be RERUN on individual communities.

--- R/analyze_rl_network.R
+++ R/analyze_rl_network.R
@@ -22,16 +22,6 @@ analyze_rl_network <- function(input, h = 8, w = 8, prefix = ""){
   tmp_net <- input
   l <- layout_nicely(tmp_net)
 
-  pdf(paste0(prefix, "Analyzed_Network.pdf"), h = h, w = w, useDingbats = FALSE)
-  plot(tmp_net, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
-  dev.off()
-
-  pdf(paste0(prefix, "Analyzed_Network_noname.pdf"), h = h, w = w, useDingbats = FALSE)
-  net2 <- tmp_net
-  V(net2)$name <- ""
-  plot(net2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
-  dev.off()
-
   tmp_net_cp <- tmp_net
   cfg <- cluster_edge_betweenness(as.undirected(tmp_net_cp))
 
@@ -46,6 +36,62 @@ analyze_rl_network <- function(input, h = 8, w = 8, prefix = ""){
 
   V(tmp_net_cp)$color <- clusts
 
+  #####
+
+  deg <- degree(tmp_net, mode="all")
+  deg <- sort(deg, decreasing = T)
+
+  deg_rec <- degree(tmp_net, mode="in")
+  deg_rec <- sort(deg_rec, decreasing = T)
+
+  deg_lig <- degree(tmp_net, mode="out")
+  deg_lig <- sort(deg_lig, decreasing = T)
+
+  vert_rank <- (betweenness(as.undirected(tmp_net))) # VERTICES
+  vert_rank_srt <- sort(vert_rank, decreasing = T)
+
+  edg_rank <- (edge_betweenness(as.undirected(tmp_net))) # EDGES
+  tmp <- igraph::as_edgelist(tmp_net)
+  tmp2 <- apply( tmp[ , 1:2 ] , 1 , paste , collapse = "-" )
+  names(edg_rank) <- tmp2
+  edg_rank <- sort(edg_rank, decreasing = T)
+
+  hs <- hub_score(tmp_net, weights=NA)$vector #Outgoing (ligands)
+  hs <- sort(hs, decreasing = T)
+
+  as <- authority_score(tmp_net, weights=NA)$vector # Incoming (receptors)
+  as <- sort(as, decreasing = T)
+
+  cluster_edge_btw <- vector(mode = "list", length = length(unique(cfg$membership)))
+  sizes <- rep(0, length(V(tmp_net)))
+
+  for (i in 1:length(unique(cfg$membership))) {
+    int <- which(cfg$membership == i)
+    vals <- vert_rank[int]
+    vals <- rank(vals)
+    size <- (3/max(vals)*vals)
+    vals2 <- sort(vals, decreasing = T)
+    cluster_edge_btw[[i]] <- vals2
+    sizes[int] <- size
+  }
+
+  V(tmp_net_cp)$size <- sizes
+
+  coGrph <- delete_edges(tmp_net, E(tmp_net)[crossing(cfg, tmp_net)])
+  comm_ind <- igraph::decompose.graph(coGrph)
+
+  #####
+
+  pdf(paste0(prefix, "Analyzed_Network.pdf"), h = h, w = w, useDingbats = FALSE)
+  plot(tmp_net, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
+  dev.off()
+
+  pdf(paste0(prefix, "Analyzed_Network_noname.pdf"), h = h, w = w, useDingbats = FALSE)
+  net2 <- tmp_net
+  V(net2)$name <- ""
+  plot(net2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = NA, cex.col= "black", rescale = TRUE)
+  dev.off()
+
   pdf(paste0(prefix, "Analyzed_Network_communities.pdf"), h = h, w = w, useDingbats = FALSE)
   plot(tmp_net_cp, mark.groups = cfg, cols_clust2 = cols_clust2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = 'black', cex.col= "black", rescale = TRUE)
   dev.off()
@@ -55,17 +101,24 @@ analyze_rl_network <- function(input, h = 8, w = 8, prefix = ""){
   plot(tmp_net_cp, mark.groups = cfg, vertex.label = "", cols_clust2 = cols_clust2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = 'black', cex.col= "black", rescale = TRUE)
   dev.off()
 
+  pdf(paste0(prefix, "Analyzed_Network_communities_crossing.pdf"), h = h, w = w, useDingbats = FALSE)
+  tmp_net_cp2 <- tmp_net_cp
+  E(tmp_net_cp2)$color <- "black"
+  E(tmp_net_cp2)$color[crossing(cfg,tmp_net_cp2)] <- "red"
+  plot(tmp_net_cp2, mark.groups = cfg, vertex.label = "", cols_clust2 = cols_clust2, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = 'black', cex.col= "black", rescale = TRUE)
+  dev.off()
+
   pdf(paste0(prefix, "Analyzed_Network_communities_numbered.pdf"), h = h, w = w, useDingbats = FALSE)
   V(tmp_net_cp)$name <- cfg$membership
   plot(tmp_net_cp, layout = l, edge.curved=curve_multiple(tmp_net), vertex.frame.color = 'black', cex.col= "black", rescale = TRUE)
   dev.off()
 
-
-  pdf(paste0(prefix, "Dendrogram.pdf"), height = 10, width = 50)
-  dendPlot(cfg, mode="hclust")
-  dev.off()
+  # pdf(paste0(prefix, "Dendrogram.pdf"), height = 10, width = 50)
+  # dendPlot(cfg, mode="hclust")
+  # dev.off()
 
   #####
+
   nodes <- igraph::as_data_frame(tmp_net, what = "vertices")
   links <- igraph::as_data_frame(tmp_net, what = "edges")
   links$arrows <- "to"
@@ -76,10 +129,7 @@ analyze_rl_network <- function(input, h = 8, w = 8, prefix = ""){
   nodes$label <- V(tmp_net)$name
   links$value <- E(tmp_net)$width
 
-  deg <- degree(tmp_net, mode="all")
-  deg <- rank(deg)
-  deg <- (20/max(deg)*deg)
-  deg[which(deg < 0.5)] <- 0.5
+  deg <- (20/max(sizes)*sizes)
   nodes$value <- deg
 
   nodes$community <- cfg$membership
@@ -96,43 +146,21 @@ analyze_rl_network <- function(input, h = 8, w = 8, prefix = ""){
 
   #####
 
-  deg <- degree(tmp_net, mode="all")
-  deg <- sort(deg, decreasing = T)
-
-  deg_rec <- degree(tmp_net, mode="in")
-  deg_rec <- sort(deg_rec, decreasing = T)
-
-  deg_lig <- degree(tmp_net, mode="out")
-  deg_lig <- sort(deg_lig, decreasing = T)
-
-  vert_rank <- (betweenness(as.undirected(tmp_net))) # VERTICES
-  vert_rank <- sort(vert_rank, decreasing = T)
-
-  edg_rank <- (edge_betweenness(as.undirected(tmp_net))) # EDGES
-  tmp <- igraph::as_edgelist(tmp_net)
-  tmp2 <- apply( tmp[ , 1:2 ] , 1 , paste , collapse = "-" )
-  names(edg_rank) <- tmp2
-  edg_rank <- sort(edg_rank, decreasing = T)
-
-  hs <- hub_score(tmp_net, weights=NA)$vector #Outgoing (ligands)
-  hs <- sort(hs, decreasing = T)
-
-  as <- authority_score(tmp_net, weights=NA)$vector # Incoming (receptors)
-  as <- sort(as, decreasing = T)
-
-  results <- vector(mode = "list", length = 7)
+  results <- vector(mode = "list", length = 10)
   results[[1]] <- deg
   results[[2]] <- deg_rec
-  results[[3]] <- vert_rank
+  results[[3]] <- vert_rank_srt
   results[[4]] <- as
   results[[5]] <- deg_lig
   results[[6]] <- edg_rank
   results[[7]] <- hs
   results[[8]] <- cfg
-  results[[9]] <- vit_net
+  results[[9]] <- cluster_edge_btw
+  results[[10]] <- comm_ind
+  results[[11]] <- vit_net
 
   names(results) <- c("Degree", "Node_degree", "Node_betweeness", "Node_authority",
-                      "Edge_degree", "Edge_betweeness", "Edge_hub", "Communities", "Interactive")
+                      "Edge_degree", "Edge_betweeness", "Edge_hub", "Communities", "Communities_betweeness", "Communities_individual", "Interactive")
   return(results)
 }
 
