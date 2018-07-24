315629aeee799ddcddc87d1eb4b05c23f5966782
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-07-23 22:50:41 -0400

    Many heatmap changes for faceting and clustering

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -13,6 +13,8 @@
 #' @param group_names whether groups should be labelled
 #' @param gene_names whether genes should be labelled
 #' @param facet_by will create breaks in the heatmap by some pData Variable
+#' @param pdf_format can be "tile" or "raster." tile is generally higher quality while raster is more efficient
+#' @param ceiling A value above which to truncate
 #' @export
 #' @details
 #' Utilize information stored in pData to control the plot display.
@@ -20,13 +22,22 @@
 #' plot_tsne_metadata(ex_sc_example, color_by = "UMI_sum", title = "UMI_sum across clusters", facet_by = "Cluster", ncol = 3)
 
 plot_heatmap <- function(input, genes, type, title = "Heatmap", scale_by = "row", cluster_by = "row", color_pal = viridis::magma(256),
-                         text_angle = 60, group_names = TRUE, gene_names = TRUE, facet_by = FALSE, ncol = 3){
+                         text_angle = 60, group_names = TRUE, gene_names = TRUE, facet_by = FALSE, pdf_format = "raster", ceiling = FALSE){
+  gg_color_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
+  }
+  if(facet_by != FALSE){
+    n = length(unique(pData(input)[,facet_by]))
+    dynamic_colors = gg_color_hue(n)
+  }
   if(type == "bulk"){
     heat_dat <- fData(input)[,grep("bulk", colnames(fData(input)))]
     heat_dat <- heat_dat[genes,]
   }
   if(type == "single_cell"){
     heat_dat <- exprs(input)[genes,]
+    metadata <- pData(input)[,facet_by]
   }
   if(scale_by == "row"){
     heat_dat_2 <- t(apply(heat_dat,1,scale))
@@ -47,6 +58,9 @@ plot_heatmap <- function(input, genes, type, title = "Heatmap", scale_by = "row"
     d <- dist(t(heat_dat), method = "euclidean")
     hc1 <- hclust(d, method = "complete" )
     heat_dat <- heat_dat[,hc1$order]
+    if(type == "single_cell"){
+      metadata <- metadata[hc1$order]
+    }
   }
   if(cluster_by == "both"){
     d <- dist(heat_dat, method = "euclidean")
@@ -55,29 +69,52 @@ plot_heatmap <- function(input, genes, type, title = "Heatmap", scale_by = "row"
     d <- dist(t(heat_dat), method = "euclidean")
     hc1 <- hclust(d, method = "complete" )
     heat_dat <- heat_dat[,hc1$order]
+    if(type == "single_cell"){
+      metadata <- metadata[hc1$order]
+    }
   }
   #####
-  if(type == "single_cell"){
-    heat_dat[which(heat_dat > 5)] <- 5
+  if(ceiling  != FALSE){
+    heat_dat[which(heat_dat > ceiling)] <- ceiling
   }
 
   heat_dat <- as.data.frame(heat_dat)
-  colnames(heat_dat) <- sub("_num_.*", "", colnames(heat_dat))
+  if(type == "bulk"){
+    colnames(heat_dat) <- sub("_num_.*", "", colnames(heat_dat))
+  }
   heat_dat_lng <- tidyr::gather(heat_dat, key = "group", "Expression", 1:ncol(heat_dat), factor_key = "TRUE")
   heat_dat_lng$genes <- rep(factor(rownames(heat_dat), levels = rownames(heat_dat)), ncol(heat_dat))
   if(facet_by != FALSE){
     facs <- unique(pData(input)[,facet_by])
+    facs <- sort(facs)
     heat_dat_lng$facet  <- NA
-    for (i in 1:length(facs)) {
-      int <- facs[i]
-      ind <- grep(paste0(int, "_"), heat_dat_lng$group)
-      heat_dat_lng$facet[ind] <- int
+    if(type == "bulk"){
+      for (i in 1:length(facs)) {
+        int <- facs[i]
+        vals <- strsplit(as.character(heat_dat_lng$group), split = "_")
+        vals <- matrix(unlist(vals), ncol = length(vals[[1]]), byrow = T)
+        for (j in 1:nrow(vals)) {
+          int2 <- vals[j,]
+          ind <- match(int, int2)
+          if(!is.na(ind)){
+            heat_dat_lng$facet[j] <- int
+          }
+        }
+      }
+      heat_dat_lng$facet <- factor(heat_dat_lng$facet)
+      colnames(heat_dat_lng)[ncol(heat_dat_lng)] <- facet_by
+    } else {
+      heat_dat_lng$facet <- rep(metadata, each = length(genes))
+      colnames(heat_dat_lng)[ncol(heat_dat_lng)] <- facet_by
     }
-    heat_dat_lng$facet <- factor(heat_dat_lng$facet)
-    colnames(heat_dat_lng)[ncol(heat_dat_lng)] <- facet_by
   }
   g <- ggplot(heat_dat_lng, aes(group, genes))
-  g <- g + geom_tile(aes(fill = Expression, colour=Expression))
+  if(pdf_format == "raster"){
+    g <- g + geom_raster(aes(fill = Expression))
+  }
+  if(pdf_format == "tile"){
+    g <- g + geom_tile(aes(fill = Expression, colour=Expression))
+  }
   g <- g + theme_classic()
   g <- g + scale_fill_gradientn(colours = color_pal)
   g <- g + scale_color_gradientn(colours = color_pal)
@@ -98,7 +135,10 @@ plot_heatmap <- function(input, genes, type, title = "Heatmap", scale_by = "row"
     g <- g + theme(axis.text.y=element_blank())
   }
   if(facet_by != FALSE){
-    g <- g + facet_wrap(reformulate(facet_by), scales = "free_x", ncol = ncol)
+    g <- g + facet_grid(reformulate(facet_by), scales = "free_x", space = "free_x")
+    # g <- g + facet_wrap(reformulate(facet_by), scales = "free_x", ncol = ncol)
+   # https://github.com/tidyverse/ggplot2/issues/2096
+    #https://stackoverflow.com/questions/19440069/ggplot2-facet-wrap-strip-color-based-on-variable-in-data-set
   }
   plot(g)
 }
