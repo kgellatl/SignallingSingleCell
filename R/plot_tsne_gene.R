a6792b6685cfe8fd0e62084e3f474e9142ed3b09
elisadonnard <12720187+elisadonnard@users.noreply.github.com>
2018-07-26 19:27:21 -0400

    made it possible to specify the tSNE coordinate columns

--- R/plot_tsne_gene.R
+++ R/plot_tsne_gene.R
@@ -12,15 +12,25 @@
 #' @param facet_by If ONE gene is being plotted it can be faceted. You cannot facet multiple genes
 #' @param ncol controls the number of columns if faceting
 #' @param size The size of the points
+#' @param xcol pData column to use for x axis
+#' @param ycol pData column to use for y axis
 #' @export
 #' @details
 #' Utilize information stored in pData to control the plot display.
 #' @examples
 #' plot_tsne_gene(ex_sc_example, gene = "Tnf", title = "Tnf over Time", facet_by = "Timepoint", density = TRUE)
 #'
-plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by = "NA",
+plot_tsne_gene <- function(input, genes,
+                           title = "",
+                           density = FALSE,
+                           facet_by = "NA",
                            colors_points = c("gray", 'blue', 'red', 'yellow'),
-                           size = 1.5, ncol = 2, resolution = 500, cutoff = 0.2){
+                           size = 1.5,
+                           ncol = 2,
+                           resolution = 500,
+                           cutoff = 0.2,
+                           xcol="x",
+                           ycol="y"){
   kde2d_weighted <- function (x, y, w, h, n , lims = c(range(x), range(y))) {
     nx <- length(x)
     if (length(y) != nx)
@@ -40,7 +50,7 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
     return(list(x = gx, y = gy, z = z))
   }
   if(facet_by == "NA"){ #This will allow plotting of 1 - n genes
-    geneColored1 <- pData(input)[,c("x", "y")]
+    geneColored1 <- pData(input)[,c(xcol, ycol)]
     geneColored1 <- do.call(rbind, replicate(length(genes), geneColored1, simplify=FALSE))
     geneColored3 <- t(exprs(input)[genes,])
     for(i in 1:ncol(geneColored3)){
@@ -60,8 +70,8 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
       for(i in 1:length(genes)){
         index <- grep(genes[i], geneColored1[,"gene"])
         subset <- geneColored1[index,]
-        dens <- kde2d_weighted(subset$x, subset$y, subset$vals, n = resolution)
-        dfdens <- data.frame(expand.grid(x=dens$x, y=dens$y), z=as.vector(dens$z))
+        dens <- kde2d_weighted(subset[,xcol], subset[,ycol], subset$vals, n = resolution)
+        dfdens <- data.frame(expand.grid(x=dens[,xcol], y=dens[,ycol]), z=as.vector(dens$z))
         dfdens_norm <- dfdens
         dfdens_norm[,3] <- dfdens_norm[,3]/max(dfdens_norm[,3])
         dfdens_norm$gene <- genes[i]
@@ -81,10 +91,10 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
     g <- g +  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5))
     g <- g +  facet_wrap(~gene, ncol = ncol)
     if(density == TRUE){
-      g <- g +  geom_point(data=final_dfdens_norm, aes(x=x, y=y, color=z), shape=20, size=.1, stroke = 1)
+      g <- g +  geom_point(data=final_dfdens_norm, aes_string(x=xcol, y=ycol, color="z"), shape=20, size=.1, stroke = 1)
     }
     g <- g +  scale_color_gradientn(colours=colors_points)
-    g <- g +  geom_point(data= geneColored1, aes(x=x, y=y, col=vals), shape=20, size = size)
+    g <- g +  geom_point(data= geneColored1, aes_string(x=xcol, y=ycol, col="vals"), shape=20, size = size)
     if(title == ""){
       title <- genes
       g <- g +  labs(title= title, col= "UMIs", x = "tSNE[1]", y = "tSNE[2]")
@@ -95,7 +105,7 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
     if(length(genes) > 1){
       stop("You cannot facet multiple genes")
     }
-    geneColored1 <- pData(input)[,c("x", "y", facet_by)]
+    geneColored1 <- pData(input)[,c(xcol, ycol, facet_by)]
     geneColored1 <- as.data.frame(geneColored1)
     values <- log2(t(exprs(input)[genes,])+2)-1
     values <- as.vector(values)
@@ -107,8 +117,8 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
       for(i in 1:length(facets)){
         index <- grep(facets[i], geneColored1[,facet_by])
         subset <- geneColored1[index,]
-        dens <- kde2d_weighted(subset$x, subset$y, subset$vals, n = resolution)
-        dfdens <- data.frame(expand.grid(x=dens$x, y=dens$y), z=as.vector(dens$z))
+        dens <- kde2d_weighted(subset[,xcol], subset[,ycol], subset$vals, n = resolution)
+        dfdens <- data.frame(expand.grid(x=dens[,xcol], y=dens[,ycol]), z=as.vector(dens$z))
         dfdens_norm <- dfdens
         dfdens_norm[,3] <- dfdens_norm[,3]/max(dfdens_norm[,3])
         dfdens_norm$facets <- facets[i]
@@ -118,22 +128,22 @@ plot_tsne_gene <- function(input, genes, title = "", density = FALSE,  facet_by 
       final_dfdens_norm <- final_dfdens_norm[-remove,]
       final_dfdens_norm <- final_dfdens_norm[with(final_dfdens_norm, order(final_dfdens_norm[,3])), ]
       geneColored1 <- geneColored1[with(geneColored1, order(geneColored1[,4])), ]
-      colnames(final_dfdens_norm) <- c("x", "y", "z", facet_by)
+      colnames(final_dfdens_norm) <- c(xcol, ycol, "z", facet_by)
     } else {
       geneColored1 <- geneColored1[with(geneColored1, order(geneColored1[,4])), ]
     }
-    tmp <- pData(input)[c("x", "y")]
+    tmp <- pData(input)[c(xcol, ycol)]
     g <- ggplot(geneColored1)
     if(density == TRUE){
       g <- g +  geom_point(data=final_dfdens_norm, aes(x=x, y=y, color=z), shape=20, size=.1, stroke = 1)
     }
-    g <- g +  geom_point(data= tmp, aes(x=x, y=y), shape=20, size = size, col = "gray")
+    g <- g +  geom_point(data= tmp, aes_string(x=xcol, y=ycol), shape=20, size = size, col = "gray")
     g <- g +  theme_classic()
     g <- g +  theme(plot.title = element_text(size = 15), axis.title = element_text(size = 10), legend.title = element_text(size = 10), legend.text=element_text(size=5))
     g <- g +  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5))
     g <- g +  facet_wrap(facets = reformulate(facet_by), ncol = ncol)
     g <- g +  scale_color_gradientn(colours=colors_points)
-    g <- g +  geom_point(data= geneColored1, aes(x=x, y=y, col=vals), shape=20, size = size)
+    g <- g +  geom_point(data= geneColored1, aes_string(x=xcol, y=ycol, col="vals"), shape=20, size = size)
     if(title == ""){
       title <- genes
       g <- g +  labs(title= title, col= "UMIs", x = "tSNE[1]", y = "tSNE[2]")
--- R/plot_tsne_metadata.R
+++ R/plot_tsne_metadata.R
@@ -10,14 +10,25 @@
 #' @param size The size of the points
 #' @param colors What colors to utilize for categorial data. Be sure it is of the proper length!
 #' @param legend_dot_size Size of dot in legend
-
+#' @param xcol pData column to use for x axis
+#' @param ycol pData column to use for y axis
 #' @export
 #' @details
 #' Utilize information stored in pData to control the plot display.
 #' @examples
 #' plot_tsne_metadata(ex_sc_example, color_by = "UMI_sum", title = "UMI_sum across clusters", facet_by = "Cluster", ncol = 3)
 
-plot_tsne_metadata <- function(input, title = "", color_by, facet_by = NA, ncol = "NA", size = 1.5, colors = NA, theme = "classic", legend_dot_size = 1.5){
+plot_tsne_metadata <- function(input,
+                               title = "",
+                               color_by,
+                               facet_by = NA,
+                               ncol = "NA",
+                               size = 1.5,
+                               colors = NA,
+                               theme = "classic",
+                               legend_dot_size = 1.5,
+                               xcol="x",
+                               ycol="y"){
 
   tmp <- pData(input)
   tmp <- tmp[sample(nrow(tmp)),]
@@ -37,14 +48,14 @@ plot_tsne_metadata <- function(input, title = "", color_by, facet_by = NA, ncol 
   g <- g + theme(plot.title = element_text(size = 20), axis.title = element_text(size = 10), legend.title = element_text(size = 15), legend.text=element_text(size=10))
   g <- g + theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5))
   if(all(is.na(facet_by)) == FALSE){
-    tmp <- pData(input)[c("x", "y")]
+    tmp <- pData(input)[c(xcol, ycol)]
     g <- g + geom_point(data = tmp, aes(x=x, y=y), shape = 20, col = "gray", size = size)
   }
   if(class(pData(input)[,color_by]) == "double" || class(pData(input)[,color_by]) == "integer" || class(pData(input)[,color_by]) == "numeric" ){
-    g <- g +  geom_point(aes_string(x = "x", y = "y", col = color_by), shape = 20, size = size)
+    g <- g +  geom_point(aes_string(x = xcol, y = ycol, col = color_by), shape = 20, size = size)
     g <- g +  scale_color_gradientn(colours=c('blue', 'red', 'yellow'))
   } else {
-    g <- g +  geom_point(aes_string(x = "x", y = "y", col = color_by), shape = 20, size = size)
+    g <- g +  geom_point(aes_string(x = xcol, y = ycol, col = color_by), shape = 20, size = size)
     if(all(is.na(colors)) == FALSE){
       g <- g + scale_color_manual(values = c(colors))
     }
