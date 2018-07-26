9394961c890315eb64dc0626ae46daa35e0afb62
elisadonnard <12720187+elisadonnard@users.noreply.github.com>
2018-07-26 14:20:52 -0400

    Changed to also allow for pre-defined number of clusters

--- R/cluster_sc.R
+++ R/cluster_sc.R
@@ -15,7 +15,7 @@
 #' @examples
 #' ex_sc_example <- cluster_sc(input = ex_sc_example, dimension = "Comp", method = "spectral", num_clust = 6)
 
-cluster_sc <- function(input, dimension, method, num_clust, s=2) {
+cluster_sc <- function(input, dimension, method, num_clust = NA, s=2) {
   if(dimension == "Comp"){
     tocluster = pData(input)[,grep("Comp", colnames(pData(input)))]
   }
@@ -35,23 +35,27 @@ cluster_sc <- function(input, dimension, method, num_clust, s=2) {
     colnames(comb) = "gamma"
     comb = comb[order(comb$gamma, decreasing = T), ,drop=F]
     comb$index = seq(nrow(comb))
-    # chose the max k from gamma distribution
-    fit = mgcv::gam(formula = gamma ~ s(index, bs="cs"), data = log10(comb[floor(0.01*nrow(comb)):nrow(comb),]+1))
-    test = log10(comb[,"index", drop=F])
-    p = predict(fit, test, type = "link", se.fit = T)
-    comb$pred = (10^predict(fit, test))-1
-    comb$residual = comb$gamma-comb$pred
-    comb$predsd = comb$pred+(s*sd(comb$residual))
-    print(ggplot(comb, aes(index, gamma)) +
-            geom_point(size = 0.5) +
-            theme_bw() +
-            scale_x_log10() +
-            scale_y_log10() +
-            geom_line(data=comb, aes(index, pred), colour="red") +
-            geom_line(data=comb, aes(index, predsd), colour="blue"))
-    cellcut = rownames(comb[comb$gamma>comb$predsd,])
+    if (is.na(num_clust)) {
+      # chose the max k from gamma distribution
+      fit = mgcv::gam(formula = gamma ~ s(index, bs="cs"), data = log10(comb[floor(0.01*nrow(comb)):nrow(comb),]+1))
+      test = log10(comb[,"index", drop=F])
+      p = predict(fit, test, type = "link", se.fit = T)
+      comb$pred = (10^predict(fit, test))-1
+      comb$residual = comb$gamma-comb$pred
+      comb$predsd = comb$pred+(s*sd(comb$residual))
+      print(ggplot(comb, aes(index, gamma)) +
+              geom_point(size = 0.5) +
+              theme_bw() +
+              scale_x_log10() +
+              scale_y_log10() +
+              geom_line(data=comb, aes(index, pred), colour="red") +
+              geom_line(data=comb, aes(index, predsd), colour="blue"))
+      cellcut = rownames(comb[comb$gamma>comb$predsd,])
+    } else {
+      cellcut = rownames(comb)[1:num_clust]
+    }
     cellidx = which(names(clust_tSNE$rho)%in%cellcut)
-    clust_tSNE = findClusters(clust_tSNE, peaks = cellidx)
+    clust_tSNE = densityClust::findClusters(clust_tSNE, peaks = cellidx)
     pData(input)$Cluster = paste0("Cluster", clust_tSNE$clusters)
   }
   return(input)
