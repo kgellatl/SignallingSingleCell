953b7a1560f126db50f9259f8da0991c0487450f
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-08-28 17:38:40 -0400

    added in searching for network objects

--- R/search_gene.R
+++ R/search_gene.R
@@ -11,7 +11,17 @@
 #' ex_sc_example <- calc_libsize(input = ex_sc_example)
 
 search_gene <- function(input, search){
-gene_set <- rownames(exprs(input))
-matches <- grep(search, gene_set, value = T)
-return(matches)
+  if(class(input)=="ExpressionSet"){
+    gene_set <- rownames(exprs(input))
+    matches <- grep(search, gene_set, value = T)
+    return(matches)
+  }
+  if(length(input$igraph_Network) > 0){
+    matches <- names(V(input$igraph_Network))[grep(search, names(V(input$igraph_Network)))]
+    return(matches)
+  }
+  if(length(input$input) > 0){
+    matches <- names(V(input$input))[grep(search, names(V(input$input)))]
+    return(matches)
+  }
 }
