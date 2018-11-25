77e3710f6a4a7e8c8011ed80a5e40181ea146bc1
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2018-11-24 22:43:12 -0500

    smarter id_rl, only finds what is in agg_bulk

--- R/id_rl.R
+++ R/id_rl.R
@@ -17,6 +17,13 @@ id_rl <- function(input){
   Receptor_Ligand_Data <- SignallingSingleCell:::Receptor_Ligand_Data
   ligs <- unique(Receptor_Ligand_Data$Ligand.ApprovedSymbol)
   recs <- unique(Receptor_Ligand_Data$Receptor.ApprovedSymbol)
+  ind <- grep("bulk", colnames(fData(input)))
+  if(length(ind) ==0){
+    stop("Calculate aggregate bulk values first")
+  }
+  mat <- fData(input)[,ind]
+  gsum <- apply(mat,1,sum)
+  expressed_genes_bulk <- names(which(gsum >0))
   expressed_genes <- rownames(fData(input))
   fData(input)$networks_Receptors <- FALSE
   fData(input)$networks_ligands <- FALSE
@@ -24,6 +31,9 @@ id_rl <- function(input){
   fData(input)$networks_receptor_to_ligs <- ""
   fData(input)$networks_ligands[which(is.na(match(expressed_genes, ligs)) == FALSE)] <- TRUE
   fData(input)$networks_Receptors[which(is.na(match(expressed_genes, recs)) == FALSE)] <- TRUE
+  true_ex <- match(expressed_genes_bulk, rownames(fData(input)))
+  fData(input)$networks_ligands[-true_ex] <- FALSE
+  fData(input)$networks_Receptors[-true_ex] <- FALSE
   for (i in 1:nrow(Receptor_Ligand_Data)) {
     int <- Receptor_Ligand_Data[i,]
     pair <- unlist(strsplit(as.character(int$Pair.Name), split = "_"))
