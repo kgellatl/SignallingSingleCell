c5b8724d760ed6f4acac8bc1771f170083f7e420
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2019-05-06 17:13:49 -0400

    intialized expression set class

--- R/objects.R
+++ R/objects.R
@@ -1,1 +1,36 @@
 ### Setup
+
+ex_sc <- setClass(Class = "ex_sc",
+
+                  slots = c(
+                    counts = "dgCMatrix",
+                    norm_counts = "dgCMatrix",
+                    pD = 'data.frame',
+                    fD = 'data.frame'),
+
+                  prototype = list(counts = Matrix::Matrix(c(0,1), nrow = 100, ncol = 10, sparse = T),
+                                   norm_counts = Matrix::Matrix(c(0,1), nrow = 100, ncol = 10, sparse = T),
+                                   pD = as.data.frame(matrix(NA, nrow = 10, ncol =2)),
+                                   fD = as.data.frame(matrix(NA, nrow = 100, ncol =2))),
+
+                  validity = function(object){
+                    errors <- character()
+                    genes <- nrow(object@counts)
+                    cells <- ncol(object@counts)
+                    if(genes != nrow(object@fD)){
+                      msg <- c("fD must contain as many rows as the input matrix has rows")
+                      errors <- c(errors,msg)
+                    }
+                    cells <- ncol(object@counts)
+                    if(cells != nrow(object@pD)){
+                      msg <- ("pD must contain as many rows as the input matrix has cols")
+                      errors <- c(errors,msg)
+                    }
+                    if (length(errors) == 0 ) TRUE else errors
+                  }
+)
+
+
+object <- new("ex_sc")
+validObject(object)
+
